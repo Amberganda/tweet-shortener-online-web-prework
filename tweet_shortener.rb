e68a4086ed9ba1226efc7c066886e4edcf08e934
Amber Taranto <ambzzzz@gmail.com>
2019-06-12 03:06:22 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,14 +1,14 @@
 def dictionary
     {
-        "hello" => "hi", 
+        "hello" => "hi",
         "to" => "2",
-        "two" => "2", 
+        "two" => "2",
         "too" => "2",
-        "for" => "4", 
-        "four" => "4", 
-        "be" => "b", 
+        "for" => "4",
+        "four" => "4",
+        "be" => "b",
         "you" => "u",
-        "at" => "@", 
+        "at" => "@",
         "and" => "&"
     }
 end
@@ -32,4 +32,3 @@ end
 def selective_tweet_shortener(length)
 
 end
-

75bfec2355142de51ddd1ec4c3a1ae7e05b2d962
Amber Taranto <ambzzzz@gmail.com>
2019-06-12 03:06:18 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,35 @@
-# Write your code here.
+def dictionary
+    {
+        "hello" => "hi", 
+        "to" => "2",
+        "two" => "2", 
+        "too" => "2",
+        "for" => "4", 
+        "four" => "4", 
+        "be" => "b", 
+        "you" => "u",
+        "at" => "@", 
+        "and" => "&"
+    }
+end
+
+def word_substituter(string)
+    words_array = string.split(" ")
+    words_array = words_array.collect do |word|
+        if dictionary.keys.include?(word)
+            dictionary[word]
+        else
+            word
+        end
+        
+    end
+end
+
+def bulk_tweet_shortener(array)
+
+end
+
+def selective_tweet_shortener(length)
+
+end
+
